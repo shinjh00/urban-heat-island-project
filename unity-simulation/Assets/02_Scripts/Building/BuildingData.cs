a8d6344b719b464922470e7272b9ef0f30d2a51d
shinjh00 <werytuiop00@gmail.com>
2026-06-24 14:04:10 +0900

    feat: SDoT 센서 데이터 모델 스크립트 생성

--- unity-simulation/Assets/02_Scripts/Building/BuildingData.cs
+++ unity-simulation/Assets/02_Scripts/Building/BuildingData.cs
@@ -12,21 +12,4 @@ public enum BuildingState
 }
 
 
-// 건물 데이터 모델
-[Serializable]
-public class BuildingData
-{
-    public string id;               // 원천도형ID
-    public string dongCode;         // 행정동코드
-    public string address;          // 주소 (동까지)
-    public string addressDetail;    // 주소 (지번)
-    public string type;             // 용도
-    public string material;         // 구조재
-    public float areaSize;          // 면적
-    public float height;            // 높이
-    public int floors;              // 층수
-    public double lat;              // 위도 (GeoJSON coordinates - y값)
-    public double lon;              // 경도 (GeoJSON coordinates - x값)
-    public List<Vector2> polygon;   // 건물 외곽선 꼭짓점들의 로컬 xz 좌표 목록
 
-}
